8d64a26b61867ba3ed2f48da1084c02707edf81f
paulsoh <paulsoh.dev@gmail.com>
2017-07-19 14:14:54 +0900

    Refactor modules for bundling

--- js/app.js
+++ js/app.js
@@ -1,125 +1,119 @@
-/*global Vue, todoStorage */
+var todoStorage = require('./store').todoStorage;
 
-(function (exports) {
+var filters = {
+  all: function (todos) {
+    return todos;
+  },
+  active: function (todos) {
+    return todos.filter(function (todo) {
+      return !todo.completed;
+    });
+  },
+  completed: function (todos) {
+    return todos.filter(function (todo) {
+      return todo.completed;
+    });
+  }
+};
 
-	'use strict';
+module.exports = new Vue({
 
-	var filters = {
-		all: function (todos) {
-			return todos;
-		},
-		active: function (todos) {
-			return todos.filter(function (todo) {
-				return !todo.completed;
-			});
-		},
-		completed: function (todos) {
-			return todos.filter(function (todo) {
-				return todo.completed;
-			});
-		}
-	};
+  // the root element that will be compiled
+  el: '.todoapp',
 
-	exports.app = new Vue({
+  // app initial state
+  data: {
+    todos: todoStorage.fetch(),
+    newTodo: '',
+    editedTodo: null,
+    visibility: 'all'
+  },
 
-		// the root element that will be compiled
-		el: '.todoapp',
+  // watch todos change for localStorage persistence
+  watch: {
+    todos: {
+      deep: true,
+      handler: todoStorage.save
+    }
+  },
 
-		// app initial state
-		data: {
-			todos: todoStorage.fetch(),
-			newTodo: '',
-			editedTodo: null,
-			visibility: 'all'
-		},
+  // computed properties
+  // http://vuejs.org/guide/computed.html
+  computed: {
+    filteredTodos: function () {
+      return filters[this.visibility](this.todos);
+    },
+    remaining: function () {
+      return filters.active(this.todos).length;
+    },
+    allDone: {
+      get: function () {
+        return this.remaining === 0;
+      },
+      set: function (value) {
+        this.todos.forEach(function (todo) {
+          todo.completed = value;
+        });
+      }
+    }
+  },
 
-		// watch todos change for localStorage persistence
-		watch: {
-			todos: {
-				deep: true,
-				handler: todoStorage.save
-			}
-		},
+  // methods that implement data logic.
+  // note there's no DOM manipulation here at all.
+  methods: {
 
-		// computed properties
-		// http://vuejs.org/guide/computed.html
-		computed: {
-			filteredTodos: function () {
-				return filters[this.visibility](this.todos);
-			},
-			remaining: function () {
-				return filters.active(this.todos).length;
-			},
-			allDone: {
-				get: function () {
-					return this.remaining === 0;
-				},
-				set: function (value) {
-					this.todos.forEach(function (todo) {
-						todo.completed = value;
-					});
-				}
-			}
-		},
+    pluralize: function (word, count) {
+      return word + (count === 1 ? '' : 's');
+    },
 
-		// methods that implement data logic.
-		// note there's no DOM manipulation here at all.
-		methods: {
+    addTodo: function () {
+      var value = this.newTodo && this.newTodo.trim();
+      if (!value) {
+        return;
+      }
+      this.todos.push({ title: value, completed: false });
+      this.newTodo = '';
+    },
 
-			pluralize: function (word, count) {
-				return word + (count === 1 ? '' : 's');
-			},
+    removeTodo: function (todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
 
-			addTodo: function () {
-				var value = this.newTodo && this.newTodo.trim();
-				if (!value) {
-					return;
-				}
-				this.todos.push({ title: value, completed: false });
-				this.newTodo = '';
-			},
+    editTodo: function (todo) {
+      this.beforeEditCache = todo.title;
+      this.editedTodo = todo;
+    },
 
-			removeTodo: function (todo) {
-				var index = this.todos.indexOf(todo);
-				this.todos.splice(index, 1);
-			},
+    doneEdit: function (todo) {
+      if (!this.editedTodo) {
+        return;
+      }
+      this.editedTodo = null;
+      todo.title = todo.title.trim();
+      if (!todo.title) {
+        this.removeTodo(todo);
+      }
+    },
 
-			editTodo: function (todo) {
-				this.beforeEditCache = todo.title;
-				this.editedTodo = todo;
-			},
+    cancelEdit: function (todo) {
+      this.editedTodo = null;
+      todo.title = this.beforeEditCache;
+    },
 
-			doneEdit: function (todo) {
-				if (!this.editedTodo) {
-					return;
-				}
-				this.editedTodo = null;
-				todo.title = todo.title.trim();
-				if (!todo.title) {
-					this.removeTodo(todo);
-				}
-			},
+    removeCompleted: function () {
+      this.todos = filters.active(this.todos);
+    }
+  },
 
-			cancelEdit: function (todo) {
-				this.editedTodo = null;
-				todo.title = this.beforeEditCache;
-			},
-
-			removeCompleted: function () {
-				this.todos = filters.active(this.todos);
-			}
-		},
-
-		// a custom directive to wait for the DOM to be updated
-		// before focusing on the input field.
-		// http://vuejs.org/guide/custom-directive.html
-		directives: {
-			'todo-focus': function (el, binding) {
-				if (binding.value) {
-					el.focus();
-				}
-			}
-		}
-	});
-
-})(window);
+  // a custom directive to wait for the DOM to be updated
+  // before focusing on the input field.
+  // http://vuejs.org/guide/custom-directive.html
+  directives: {
+    'todo-focus': function (el, binding) {
+      if (binding.value) {
+        el.focus();
+      }
+    }
+  }
+});
--- js/routes.js
+++ js/routes.js
@@ -1,25 +1,21 @@
 /*global app, Router */
 
-(function (app, Router) {
+var app = require('./app');
 
-	'use strict';
+var router = new Router();
 
-	var router = new Router();
+['all', 'active', 'completed'].forEach(function (visibility) {
+  router.on(visibility, function () {
+    app.visibility = visibility;
+  });
+});
 
-	['all', 'active', 'completed'].forEach(function (visibility) {
-		router.on(visibility, function () {
-			app.visibility = visibility;
-		});
-	});
+router.configure({
+  notfound: function () {
+    window.location.hash = '';
+    app.visibility = 'all';
+  }
+});
 
-	router.configure({
-		notfound: function () {
-			window.location.hash = '';
-			app.visibility = 'all';
-		}
-	});
-
-	router.init();
-  console.log("Router Intitiated!!!!!!!");
-
-})(app, Router);
+router.init();
+console.log("Router Intitiated!!!!");
